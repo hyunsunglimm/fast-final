ed88164743de9a5b5bcb0b13fcb17ce2666ec846
oyjbaam <oyjbaam@gmail.com>
2024-05-09 22:40:05 +0900

    chore: Card 내보내기 변경

--- components/ui/card/CardFooter.tsx
+++ components/ui/card/CardFooter.tsx
@@ -1,11 +1,9 @@
 import { forwardRef, HTMLAttributes } from 'react';
 import { cn } from '@/utils/twMerge';
 
-const CardFooter = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+export const CardFooter = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => {
     return <div ref={ref} className={cn('flex items-center p-5 pt-0', className)} {...props} />;
   }
 );
 CardFooter.displayName = 'CardFooter';
-
-export default CardFooter;
