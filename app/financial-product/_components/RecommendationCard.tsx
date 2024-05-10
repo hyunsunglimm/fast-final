bdc46e9ff9c16c625ed3c7ce7b7ebf2ab878aced
hyunsunglimm <gustjd3834@naver.com>
2024-05-10 20:03:05 +0900

    feat: 금융 상품 홈 이미지 추가

--- app/financial-product/_components/RecommendationCard.tsx
+++ app/financial-product/_components/RecommendationCard.tsx
@@ -1,6 +1,7 @@
 import Button from '@/components/ui/Button';
 import Text from '@/components/ui/Text';
 import { Card } from '@/components/ui/card/Card';
+import Image from 'next/image';
 
 type RecommendationInfoProps = {
   recommendationInfo: {
@@ -16,17 +17,19 @@ const RecommendationCard = ({
 }: RecommendationInfoProps) => {
   return (
     <Card className='p-[2.4rem]'>
-      <Text variant='h2' sizes='caption12' className='mb-[9px] text-[#0089EC]'>
+      <Text variant='h2' sizes='caption12' className='mb-[0.9rem] text-[#0089EC]'>
         {title}
       </Text>
-      <Text variant='p' sizes='title20' className='mb-[4px]'>
+      <Text variant='p' sizes='title20' className='mb-[0.4rem]'>
         {subTitle}
       </Text>
-      <Text variant='p' sizes='caption12' className='mb-[28px] text-[#787878]'>
+      <Text variant='p' sizes='caption12' className='mb-[2.8rem] text-[#787878]'>
         {description}
       </Text>
-      <div className='mb-[28px] flex h-[142px] items-center justify-center bg-green-300'>
-        이미지
+      <div className='mb-[2.8rem] flex justify-center'>
+        <div className='relative inset-0 h-[14.2rem] w-[17rem]'>
+          <Image src='/images/search-products.png' alt='search products' fill />
+        </div>
       </div>
       <Button>{linkTitle}</Button>
     </Card>
