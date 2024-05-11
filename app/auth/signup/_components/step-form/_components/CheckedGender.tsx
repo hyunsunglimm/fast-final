40770936ea27ee0cddcae5c44148195541c74e25
oyjbaam <oyjbaam@gmail.com>
2024-05-11 11:13:20 +0900

    refactor: 성별체크 라벨 컴포넌트로 이동

--- app/auth/signup/_components/step-form/_components/CheckedGender.tsx
+++ app/auth/signup/_components/step-form/_components/CheckedGender.tsx
@@ -1,6 +1,7 @@
 import { CardContent } from '@/components/ui/card';
 import { UseFormRegister } from 'react-hook-form';
 import { InputValueType } from '../StepFourForm';
+import Text from '@/components/ui/Text';
 
 type CheckedGenderProps = {
   register: UseFormRegister<InputValueType>;
@@ -11,34 +12,37 @@ const genderChoiceBtnClass =
 
 const CheckedGender = ({ register }: CheckedGenderProps) => {
   return (
-    <CardContent justifyContent='center' className='w-full gap-x-4'>
-      <input
-        {...register('gender', { required: true })}
-        type='radio'
-        className='peer/male hidden'
-        id='male'
-        value='male'
-      />
-      <label
-        htmlFor='male'
-        className={`${genderChoiceBtnClass} peer-checked/male:bg-black peer-checked/male:text-white`}
-      >
-        남성
-      </label>
-      <input
-        {...register('gender', { required: true })}
-        type='radio'
-        id='female'
-        value='female'
-        className='peer/female hidden'
-      />
-      <label
-        htmlFor='female'
-        className={`${genderChoiceBtnClass} peer-checked/female:bg-black peer-checked/female:text-white`}
-      >
-        여성
-      </label>
-    </CardContent>
+    <>
+      <Text sizes='title20'>성별을 선택해주세요.</Text>
+      <CardContent justifyContent='center' className='w-full gap-x-4'>
+        <input
+          {...register('gender', { required: true })}
+          type='radio'
+          className='peer/male hidden'
+          id='male'
+          value='male'
+        />
+        <label
+          htmlFor='male'
+          className={`${genderChoiceBtnClass} peer-checked/male:bg-black peer-checked/male:text-white`}
+        >
+          남성
+        </label>
+        <input
+          {...register('gender', { required: true })}
+          type='radio'
+          id='female'
+          value='female'
+          className='peer/female hidden'
+        />
+        <label
+          htmlFor='female'
+          className={`${genderChoiceBtnClass} peer-checked/female:bg-black peer-checked/female:text-white`}
+        >
+          여성
+        </label>
+      </CardContent>
+    </>
   );
 };
 
