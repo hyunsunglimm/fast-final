6daed273c294daa8c5634a53305d7b17ad808846
oyjbaam <oyjbaam@gmail.com>
2024-05-11 17:21:40 +0900

    feat: SignupForm 퍼널 적용

--- app/auth/signup/_components/SignupForm.tsx
+++ app/auth/signup/_components/SignupForm.tsx
@@ -1,10 +1,31 @@
-import { StepOneForm, StepTwoForm } from './step-form';
+'use client';
+import { StepOneForm, StepTwoForm, StepFourForm, StepFiveForm } from './step-form';
+import { useFunnel } from '@/hooks/useFunnel';
+const signupSteps = ['id', 'name', '3', 'address', '5'];
 const SignupForm = () => {
+  const { Funnel, ShowStep, setStep } = useFunnel('signup-steps', signupSteps[0]);
+
   return (
-    <div>
-      <StepOneForm />
-      <StepTwoForm />
-    </div>
+    <Funnel>
+      {/* <ShowStep stepsElement='id'>
+        <StepOneForm nextStep={() => setStep('name')} />
+      </ShowStep>
+      <ShowStep stepsElement='name'>
+        <StepTwoForm nextStep={() => setStep('address')} />
+      </ShowStep> */}
+      <ShowStep stepsElement={signupSteps[3]}>
+        <StepFourForm nextStep={() => setStep(signupSteps[4])} />
+      </ShowStep>
+      <ShowStep stepsElement={signupSteps[4]}>
+        <StepFiveForm />
+      </ShowStep>
+    </Funnel>
   );
 };
 export default SignupForm;
+
+// {(step === '1' || step === undefined) && <StepOneForm />}
+// {step === '2' && <StepTwoForm />}
+/* {step === '3' && <StepFourForm />} */
+// {step === '4' && <StepFourForm />}
+// {step === '5' && <StepFiveForm />}
