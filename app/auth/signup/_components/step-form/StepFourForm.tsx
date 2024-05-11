c4acaf7098f85b706bc2792940f4bd909e82f542
oyjbaam <oyjbaam@gmail.com>
2024-05-11 11:40:45 +0900

    feat: 회원가입 주소,성별 선택 스텝 폼 작성

--- app/auth/signup/_components/step-form/StepFourForm.tsx
+++ app/auth/signup/_components/step-form/StepFourForm.tsx
@@ -5,62 +5,74 @@ import React, { useState } from 'react';
 import DaumAddress from './_components/DaumAddress';
 import Text from '@/components/ui/Text';
 import CheckedGender from './_components/CheckedGender';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-type InputValueType = {
+export type InputValueType = {
   address: string;
   detailAdress?: string;
   gender: string;
 };
 
 export const StepFourForm = () => {
-  const [stepFourInput, setStepFourInput] = useState<InputValueType>({
-    address: '',
-    detailAdress: '',
-    gender: ''
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+    setValue,
+    getValues
+  } = useForm<InputValueType>({ mode: 'onChange' });
   const [visiblePostDaum, setVisiblePostDaum] = useState(false);
+  const addressFieldValues = getValues('address');
 
-  const handlePostDaumVisible = () => {
-    setVisiblePostDaum((prev) => !prev);
-  };
+  const nextStepSubmit: SubmitHandler<InputValueType> = (data) => {};
 
   return (
     <CardContent flexDirection='col' className='relative h-screen w-full'>
       {visiblePostDaum && (
-        <DaumAddress setVisiblePostDaum={setVisiblePostDaum} setStepFourInput={setStepFourInput} />
+        <DaumAddress setValue={setValue} setVisiblePostDaum={setVisiblePostDaum} />
       )}
+      <form className='w-full' onSubmit={handleSubmit(nextStepSubmit)}>
+        <label htmlFor='address'>
+          <Text sizes='title20'>주소를 입력해주세요.</Text>
+        </label>
+        <CardContent className='w-full'>
+          <input
+            {...register('address', { required: true })}
+            placeholder='주소를 입력해주세요.'
+            aria-label='주소를 입력해주세요.'
+            type='text'
+            id='address'
+            className='h-10 w-full flex-grow'
+            defaultValue={addressFieldValues || ''}
+          />
+          <Button type='button' onClick={() => setVisiblePostDaum((prev) => !prev)}>
+            주소 검색
+          </Button>
+        </CardContent>
+        {Boolean(addressFieldValues) && (
+          <>
+            <label htmlFor='detailAddress'>
+              <Text sizes='title20'>상세 주소를 입력해주세요.</Text>
+            </label>
+            <input
+              {...register('detailAdress')}
+              placeholder='상세주소를 입력해주세요.'
+              type='text'
+              id='detailAdress'
+              className='h-10 w-full'
+            />
+          </>
+        )}
 
-      <label htmlFor='address'>
-        <Text sizes='title20'>주소를 입력해주세요.</Text>
-      </label>
-      <CardContent className='w-full'>
-        <input
-          placeholder='주소를 입력해주세요.'
-          aria-label='주소를 입력해주세요.'
-          type='text'
-          id='address'
-          className='h-10 w-full flex-grow'
-          defaultValue={stepFourInput.address && stepFourInput.address}
-        />
-        <Button onClick={handlePostDaumVisible}>주소 검색</Button>
-      </CardContent>
-      {stepFourInput.address && (
-        <>
-          <label htmlFor='detailAddress'>
-            <Text sizes='title20'>상세 주소를 입력해주세요.</Text>
-          </label>
-          <input placeholder='선택사항' type='text' id='address' className='h-10 w-full' />
-        </>
-      )}
-
-      <Text sizes='title20'>성별을 선택해주세요.</Text>
-      <CheckedGender setStepFourInput={setStepFourInput} />
-      <Button
-        disabled={!(!!stepFourInput.address && !!stepFourInput.gender)}
-        className='disabled:cursor-not-allowed disabled:bg-gray-300'
-      >
-        다음
-      </Button>
+        <CheckedGender register={register} />
+        <Button
+          type='submit'
+          disabled={!isValid}
+          className='disabled:cursor-not-allowed disabled:bg-gray-300'
+        >
+          다음
+        </Button>
+      </form>
     </CardContent>
   );
 };
