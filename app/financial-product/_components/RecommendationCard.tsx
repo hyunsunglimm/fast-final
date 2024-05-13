084ad3807e916827166bf67192c27c82b5bef587
hyunsunglimm <gustjd3834@naver.com>
2024-05-13 14:05:05 +0900

    feat: 상품 탐색 홈에서 링크 추가

--- app/financial-product/_components/RecommendationCard.tsx
+++ app/financial-product/_components/RecommendationCard.tsx
@@ -2,6 +2,7 @@ import Button from '@/components/ui/Button';
 import Text from '@/components/ui/Text';
 import { Card } from '@/components/ui/card/Card';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type RecommendationInfoProps = {
   recommendationInfo: {
@@ -9,11 +10,12 @@ type RecommendationInfoProps = {
     subTitle: string;
     description: string;
     linkTitle: string;
+    href: string;
   };
 };
 
 const RecommendationCard = ({
-  recommendationInfo: { title, subTitle, description, linkTitle }
+  recommendationInfo: { title, subTitle, description, linkTitle, href }
 }: RecommendationInfoProps) => {
   return (
     <Card className='p-[2.4rem]'>
@@ -28,10 +30,18 @@ const RecommendationCard = ({
       </Text>
       <div className='mb-[2.8rem] flex justify-center'>
         <div className='relative inset-0 h-[14.2rem] w-[17rem]'>
-          <Image src='/images/search-products.png' alt='search products' fill />
+          <Image
+            src='/images/search-products.png'
+            alt='search products'
+            fill
+            sizes='auto'
+            priority
+          />
         </div>
       </div>
-      <Button>{linkTitle}</Button>
+      <Link href={`/financial-product/${href}`}>
+        <Button>{linkTitle}</Button>
+      </Link>
     </Card>
   );
 };
