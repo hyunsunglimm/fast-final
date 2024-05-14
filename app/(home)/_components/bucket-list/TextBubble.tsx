0f22926f9ff703806f48ad4e79159ee8af3e8e48
oyjbaam <oyjbaam@gmail.com>
2024-05-14 22:23:31 +0900

    chore: 파일 위치 이동

--- app/(home)/_components/bucket-list/TextBubble.tsx
+++ app/(home)/_components/bucket-list/TextBubble.tsx
@@ -1,13 +1,14 @@
 import Text from '@/components/ui/Text';
 import FlexBox from '@/components/ui/FlexBox';
-import '@/app/globals.css';
+
 type TextBubbleProps = {
   amounts: number;
   date: string;
   className?: string;
   isChallenge: boolean;
 };
-const TextBubble = ({ amounts, date, className, isChallenge }: TextBubbleProps) => {
+
+export const TextBubble = ({ amounts, date, className, isChallenge }: TextBubbleProps) => {
   const txtColor = isChallenge ? 'text-[#FF8B4A]' : 'text-[#4AA8FF]';
   const afterClass =
     'after:absolute after:bottom-[-1rem] after:left-1/4 after:h-[1rem] after:w-[1rem] after:border-b-[0.5rem] after:border-l-[0.5rem] after:border-r-[0.5rem] after:border-t-[0.5rem] after:border-b-transparent after:border-l-transparent after:border-r-transparent after:border-t-white';
@@ -25,4 +26,3 @@ const TextBubble = ({ amounts, date, className, isChallenge }: TextBubbleProps) 
     </FlexBox>
   );
 };
-export default TextBubble;
