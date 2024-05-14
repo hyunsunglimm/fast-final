ad4ab1621b7e6af29764c8d39bc5b8de1c87df36
oyjbaam <oyjbaam@gmail.com>
2024-05-14 16:34:19 +0900

    refactor: Card shadow 변경, tailwind GrayScale 시스템 적용, body backgroundColor 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,15 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        '3xl': '0px 4px 10px 0px rgba(0, 0, 0, 0.06)'
+      },
       screens: {
         xs: '520px'
-      },
-      backgroundColor: {
-        'gray-100': '#f0f0f0',
-        'gray-200': '#e8e8e8',
-        'gray-300': '#d0d0d0',
-        'gray-500': '#949494',
-        'gray-700': '#505050'
       },
       fontSize: {
         '48': ['4.8rem', { lineHeight: '7.2rem' }],
@@ -34,7 +30,17 @@ const config: Config = {
       },
       colors: {
         active: '#0089EC',
-        red_500: '#F5553F'
+        red_500: '#F5553F',
+        'gray-10': '#F7F9FA',
+        'gray-50': '#F2F4F6',
+        'gray-100': '#EDF0F3',
+        'gray-200': '#E4E9EF',
+        'gray-300': '#CCD1D6',
+        'gray-400': '#B8BEC4',
+        'gray-500': '#8E939A',
+        'gray-600': '#747A81',
+        'gray-700': '#4B5158',
+        black: '#23282E'
       }
     }
   },
