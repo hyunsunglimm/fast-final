58fe1a43b9b0b2d66b0b0bfe71c48d537bc19076
oyjbaam <oyjbaam@gmail.com>
2024-05-14 20:48:55 +0900

    feat: 홈 배너 영역 컴포넌트

---
+++ app/(home)/_components/HomeBanner.tsx
@@ -0,0 +1,7 @@
+import FlexBox from '@/components/ui/FlexBox';
+import React from 'react';
+
+const HomeBanner = () => {
+  return <FlexBox className='my-[2rem] h-[10.1rem] rounded-3xl bg-gray-300'>HomeBanner</FlexBox>;
+};
+export default HomeBanner;
