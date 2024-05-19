f547c26a78b4dc2bc480d93126190307c594f1ae
oyjbaam <oyjbaam@gmail.com>
2024-05-19 20:48:30 +0900

    add: 각 위젯 파일 생성

---
+++ app/(home)/_components/widget-item/WidgetUpcomingExpenses.tsx
@@ -0,0 +1,6 @@
+// 다가오는 지출
+import React from 'react';
+import { WidgetItemCard } from './WidgetItemCard';
+export const WidgetUpcomingExpenses = () => {
+  return <WidgetItemCard title='다가오는 지출'>WidgetUpcoming</WidgetItemCard>;
+};
