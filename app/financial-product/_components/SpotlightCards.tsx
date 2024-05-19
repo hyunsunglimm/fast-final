87f7275b3120fc90473c0baf9f6330c562494b43
hyunsunglimm <gustjd3834@naver.com>
2024-05-19 22:41:10 +0900

    feat: 금융상품 탐색 홈 >  주목받는 카드 레이아웃 구조 작성

--- app/financial-product/_components/SpotlightCards.tsx
+++ app/financial-product/_components/SpotlightCards.tsx
@@ -5,6 +5,45 @@ import FlexBox from '@/components/ui/FlexBox';
 import Tab from '@/components/ui/Tab';
 import Text from '@/components/ui/Text';
 import { useState } from 'react';
+import SpotlightCardItem from './SpotlightCardItem';
+
+const spotlightCards = [
+  {
+    title: '카카오뱅크 신한카드 Time',
+    annualBenefits: 100602,
+    annualFee: '연회비 1만원',
+    image: 'kakao-sinhan-time',
+    isEvent: false
+  },
+  {
+    title: '신한카드 Mr.Life',
+    annualBenefits: 91162,
+    annualFee: '연회비 1만 5천원',
+    image: 'sinhan-mrlife',
+    isEvent: false
+  },
+  {
+    title: '카카오페이 신한 라이언',
+    annualBenefits: 80000,
+    annualFee: '연회비 이벤트',
+    image: 'kakao-sinhan-ryan',
+    isEvent: true
+  },
+  {
+    title: 'BC 바로 클리어 플러스',
+    annualBenefits: 71165,
+    annualFee: '연회비 1만원',
+    image: 'bc-clear-plus',
+    isEvent: false
+  },
+  {
+    title: 'BC 바로 클리어',
+    annualBenefits: 56428,
+    annualFee: '연회비 이벤트',
+    image: 'bc-clear',
+    isEvent: true
+  }
+];
 
 const SpotlightCards = () => {
   const [selectedTab, setSelectedTab] = useState('신용카드');
@@ -24,12 +63,23 @@ const SpotlightCards = () => {
           주목받는 카드
         </Text>
       </FlexBox>
-      <Tab
-        array={['신용카드', '체크카드']}
-        type='box'
-        selectedTab={selectedTab}
-        setSelectedTab={setSelectedTab}
-      />
+      <div className='mb-24 mt-16'>
+        <Tab
+          array={['신용카드', '체크카드']}
+          type='box'
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        />
+      </div>
+      <ul className='flex flex-col gap-[1.2rem]'>
+        {spotlightCards.map((card, index) => {
+          return (
+            <li key={card.title}>
+              <SpotlightCardItem card={card} count={index + 1} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
