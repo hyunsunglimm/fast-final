4d0b3ef84c99d0a5020b6825032df974a063bbcd
hyunsunglimm <gustjd3834@naver.com>
2024-05-20 18:14:51 +0900

    feat: Navbar 수정
    금융상품 탐색 페이지 이동 시 쿼리파라미터 설정

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ export const NAV_DATA = [
   { title: '홈', path: '/', icon: <HomeIcon /> },
   { title: '자산', path: '/asset-management', icon: <AssetManagementIcon /> },
   { title: '가계부', path: '/budget-calendar', icon: <BudgetCalendarIcon /> },
-  { title: '상품 탐색', path: '/financial-product', icon: <FinancialProductIcon /> },
+  { title: '상품 탐색', path: '/financial-product?tab=신용카드', icon: <FinancialProductIcon /> },
   { title: '전체', path: '#', icon: <NavAllIcon /> }
 ];
 
@@ -29,7 +29,9 @@ const Navbar = () => {
           <ul className='mt-10 flex items-center justify-between'>
             {NAV_DATA.map((nav) => {
               const isActive =
-                nav.path === '/' ? pathname === nav.path : pathname.startsWith(nav.path);
+                nav.path === '/'
+                  ? pathname === nav.path
+                  : pathname.startsWith(nav.path.split('?')[0]);
               const activeClass = isActive ? 'text-primary' : 'text-gray-500';
 
               return (
