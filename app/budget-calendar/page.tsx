864b6008e94ec0f029763af578ab75d7e503e453
oyjbaam <oyjbaam@gmail.com>
2024-05-20 18:25:20 +0900

    refactor: input design

--- app/budget-calendar/page.tsx
+++ app/budget-calendar/page.tsx
@@ -1,14 +1,31 @@
 'use client';
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import Input from '@/components/ui/Input';
 import Checkbox from '@/components/ui/CheckBox';
 import Switch from '@/components/ui/Switch';
-
+import { useForm } from 'react-hook-form';
 const BudgetCalendarPage = () => {
   const [isChecked1, setIsChecked1] = useState(false);
   const [isChecked2, setIsChecked2] = useState(false);
   const [isChecked3, setIsChecked3] = useState(false);
+  const [input, setInput] = useState('');
+  const [errorMessage, setErrorMessage] = useState(false);
 
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, validatingFields }
+  } = useForm({ defaultValues: { memberId: '' } });
+  // console.log('validatingFields', validatingFields);
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    setInput(newValue);
+
+    // Validate input length
+    const isValidLength = newValue.length >= 4 && newValue.length <= 8;
+
+    setErrorMessage(!isValidLength);
+  };
   const handleCheckboxChange1 = (checked: boolean) => {
     setIsChecked1(checked);
   };
@@ -21,11 +38,44 @@ const BudgetCalendarPage = () => {
     setIsChecked3(!isChecked3);
   };
 
+  const onSubmit = (data: any) => {
+    // console.log(data);
+  };
+  // console.log('errors.memberId', errors.memberId);
   return (
     <div>
       <p>가계부</p>
-      <Input placeholder='Enter text...' />
-      <Input placeholder='Enter text...' borderType='bottom' action='error' />
+      <form className='px-20' onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          {...register('memberId', {
+            required: '패스워드는 필수 입력값입니다.',
+            maxLength: {
+              value: 8,
+              message: '패스워드는 4~8자리 글자 사이로 입력해주세요'
+            },
+            minLength: {
+              value: 4,
+              message: '패스워드는 4~8자리 글자 사이로 입력해주세요'
+            }
+          })}
+          id='test'
+          placeholder='아이디를 입력해주세요'
+          validation={errors.memberId ? 'error' : 'success'}
+          // onChange={handleInput}
+          // disabled
+        />
+        <Input
+          onChange={handleInput}
+          id='memberId'
+          placeholder='아이디를 입력해주세요'
+          validation={errorMessage ? 'error' : 'success'}
+        />
+        {errors.memberId && <p>{errors.memberId?.message}</p>}
+        <button className='h-10 w-10 bg-white' type='submit'>
+          dd
+        </button>
+      </form>
+      {/* <Input placeholder='Enter text...' /> */}
       <Checkbox id='1' checked={isChecked1} onChange={handleCheckboxChange1} onImage='greenImage'>
         <p>체크박스 레이블</p>
       </Checkbox>
--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -3,61 +3,47 @@ import React, { forwardRef } from 'react';
 import { cn } from '@/utils/twMerge';
 
 // Input 컴포넌트의 스타일 변형을 정의합니다.
-const inputVariants = cva('placeholder-black-opacity-30 outline-none', {
-  variants: {
-    size: {
-      sm: 'input-sm',
-      md: 'w-full h-[5.6rem] border-black px-[1.6rem]',
-      lg: 'input-lg'
+
+const inputVariants = cva(
+  'peer block w-full border-gray-500 appearance-none border-0 border-b-2 px-2.5 pb-2.5 pt-5 text-16 focus:outline-none focus:ring-0 bg-transparent disabled:cursor-not-allowed disabled:border-b-gray-300',
+  {
+    variants: {
+      validation: {
+        success: 'focus:border-active focus:text-active',
+        error: 'focus:border-warning focus:text-warning'
+      }
     },
-    radius: {
-      sm: 'rounded-sm',
-      md: 'rounded-md',
-      lg: 'rounded-lg'
-    },
-    borderType: {
-      all: 'border',
-      all2: 'border-2',
-      bottom: 'border-b rounded-none',
-      bottom2: 'border-b-2 rounded-none',
-      none: 'border-none'
-    },
-    fontSize: {
-      sm: 'text-sm',
-      md: 'text-20',
-      lg: 'text-lg'
-    },
-    action: {
-      active: 'focus:border-active',
-      error: 'focus:border-red-500'
-    }
-  },
-  defaultVariants: {
-    size: 'md',
-    radius: 'md',
-    borderType: 'all',
-    fontSize: 'md',
-    action: 'active'
+    defaultVariants: {},
+    compoundVariants: []
   }
-});
+);
 
-type BoxVariantSize = 'sm' | 'md' | 'lg';
-
-type InputProps = {
-  size?: BoxVariantSize;
-  radius?: BoxVariantSize;
-  borderType?: 'all' | 'all2' | 'bottom' | 'bottom2' | 'none';
-  fontSize?: 'sm' | 'md' | 'lg';
-  action?: 'active' | 'error';
-  className?: string;
-} & VariantProps<typeof inputVariants> &
-  React.InputHTMLAttributes<HTMLInputElement>;
+type InputProps = VariantProps<typeof inputVariants> & React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ size, radius, borderType, fontSize, action, className, ...props }: InputProps, ref) => {
-    const inputClass = cn(inputVariants({ size, radius, borderType, fontSize, action, className }));
-
-    return <input {...props} ref={ref} className={inputClass} />;
+  ({ id, placeholder, validation, className, ...props }: InputProps, ref) => {
+    return (
+      <div className='relative'>
+        <input
+          {...props}
+          ref={ref}
+          id={id}
+          className={cn(inputVariants({ className, validation }))}
+          placeholder=' '
+        />
+        <label
+          htmlFor={id}
+          className={cn(
+            'absolute start-2.5 top-5 z-10 origin-[0] -translate-y-8 scale-75 transform text-16 duration-300',
+            validation === 'success' && 'peer-focus:text-active',
+            validation === 'error' && 'peer-focus:text-warning',
+            'peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-8 peer-focus:scale-75 peer-disabled:text-gray-300 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4'
+          )}
+        >
+          {placeholder}
+        </label>
+      </div>
+    );
   }
 );
 
