dfab7c253a6e37e3d4a0b77d8c2b284a8017a9b2
hyunsunglimm <gustjd3834@naver.com>
2024-05-20 16:56:45 +0900

    feat: 금융상품 홈 레이아웃 수정

--- app/financial-product/page.tsx
+++ app/financial-product/page.tsx
@@ -1,14 +1,20 @@
+import { DefaultHeader } from '@/components/Header';
 import CustomNavigation from './_components/CustomNavigation';
 import FinancialProducts from './_components/FinancialProducts';
 import SpotlightCards from './_components/SpotlightCards';
+import FinancialProductBanner from './_components/FinancialProductBanner';
 
 const FinancialProductPage = () => {
   return (
-    <section className='px-20'>
-      <CustomNavigation />
-      <FinancialProducts />
-      <SpotlightCards />
-    </section>
+    <>
+      <DefaultHeader title='금융상품' />
+      <section className='px-20 pb-[13.2rem]'>
+        <FinancialProductBanner />
+        <CustomNavigation />
+        <FinancialProducts />
+        <SpotlightCards />
+      </section>
+    </>
   );
 };
 export default FinancialProductPage;
