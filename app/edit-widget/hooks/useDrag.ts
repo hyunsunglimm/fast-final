a69d87909b898db48b571c7c7dc628814b0904f6
oyjbaam <oyjbaam@gmail.com>
2024-05-21 23:05:18 +0900

    refactor: 드래그, 삭제 핸들러 훅으로 분리

---
+++ app/edit-widget/hooks/useDrag.ts
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import {
+  DragEndEvent,
+  DragStartEvent,
+  PointerSensor,
+  TouchSensor,
+  UniqueIdentifier,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core';
+import { arrayMove } from '@dnd-kit/sortable';
+import type { SetShowWidgetType } from '@/types/widget-type/widgetType';
+
+const useDrag = (setShowWidget: SetShowWidgetType) => {
+  const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
+  const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
+  const handleDragStart = (event: DragStartEvent) => {
+    const { active } = event;
+    const { id } = active;
+    setActiveId(id);
+  };
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (!over) return;
+    if (active.id !== over.id) {
+      setShowWidget((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  };
+
+  return { activeId, sensors, handleDragStart, handleDragEnd };
+};
+export default useDrag;
