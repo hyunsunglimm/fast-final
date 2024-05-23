0794220c28f63362cf7592b3b9aff2ffe1b81e3b
jiyoon29 <yoon__29@naver.com>
2024-05-23 17:49:41 +0900

    feat: 소비 날씨 layout

--- app/budget-calendar/page.tsx
+++ app/budget-calendar/page.tsx
@@ -26,6 +26,14 @@ const items = [
   { profile: '/icons/profile/profile.svg', name: 'David' }
 ];
 
+// 소비 날씨
+const weatherData = [
+  { src: '/icons/weather/weather-3.svg', alt: '흐림', days: '11일' },
+  { src: '/icons/weather/weather-2.svg', alt: '맑음', days: '9일' },
+  { src: '/icons/weather/weather-5.svg', alt: '천둥번개', days: '5일' },
+  { src: '/icons/weather/weather-1.svg', alt: '무지개', days: '2일' }
+];
+
 const BudgetCalendarPage = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -250,6 +258,44 @@ const BudgetCalendarPage = () => {
             <h2 className='mb-16 text-18 font-600'>{selectedProfile}님의 공유 가계부</h2>
             <div className='h-[30rem]'>캘린더 넣을 예정</div>
           </div>
+          {/* 라인 */}
+          <div className='h-[1.6rem] bg-gray-50'></div>
+          {/* 소비 날씨 */}
+          <div className='px-20 py-32 text-12'>
+            <FlexBox justifyContent='between' alignItems='center'>
+              <h2 className='mb-16 text-18 font-600'>{selectedProfile}님의 소비 날씨는?</h2>
+              <Icon size='16' src='/icons/system-icon/info.svg' alt='정보' />
+            </FlexBox>
+            <FlexBox justifyContent='between' className='my-40 rounded-md bg-gray-300 p-24'>
+              <div>
+                <h4 className='mb-8 flex h-[5.4rem] items-center text-14 font-700'>
+                  지출 비가 내린 날이
+                  <br />
+                  15일로 가장 많았어요
+                </h4>
+                <p className='text-gray-600'>목표 예산에서 평균 80% 썼어요</p>
+              </div>
+              <div className='p-[1.33rem]'>
+                <Icon
+                  src='/icons/weather/weather-4.svg'
+                  alt='비 날씨'
+                  className='h-[5.33rem] w-[5.33rem]'
+                />
+              </div>
+            </FlexBox>
+            <FlexBox justifyContent='center' className='gap-24 text-center'>
+              {weatherData.map((weather, index) => {
+                return (
+                  <div key={index} className='p-8 pb-8'>
+                    <Icon src={weather.src} alt={weather.alt} size='32' />
+                    <p className='mt-12'>{weather.days}</p>
+                  </div>
+                );
+              })}
+            </FlexBox>
+          </div>
+          {/* 라인 */}
+          <div className='h-[1.6rem] bg-gray-50'></div>
         </>
       )}
     </div>
