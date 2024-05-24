100aae443d79087ab25b98e8d48b2e354125c7f8
oyjbaam <165442923+oyjbaam@users.noreply.github.com>
2024-05-24 19:39:58 +0900

    fix release (#94)
    * fix: env err (#93)
    
    * fix: release env (#91)
    
    * fix: env
    
    * fix: error
    
    * fix: error env.
    
    ---------
    
    Co-authored-by: hyunsunglimm <gustjd3834@naver.com>
    Co-authored-by: jiyoon29 <yoon__29@naver.com>
    Co-authored-by: jiyoon29 <71622691+jiyoon29@users.noreply.github.com>

--- service/api/auth/index.ts
+++ service/api/auth/index.ts
@@ -8,5 +8,6 @@ type Tdata = {
 
 export const checkEmailDuplicate = (id: string): Promise<Tdata> => {
   const body = { id };
+
   return requestFetch(authApi.SIGN_UP, { method: 'post', body: JSON.stringify(body) });
 };
