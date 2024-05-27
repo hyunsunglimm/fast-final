3eff05611babf7fbd05d02b146841219503c95a1
hyunsunglimm <gustjd3834@naver.com>
2024-05-27 15:05:21 +0900

    feat: 말풍선 위치 조정

--- app/financial-product/comparison/select-category/result/_components/SpeechBubbleHeader.tsx
+++ app/financial-product/comparison/select-category/result/_components/SpeechBubbleHeader.tsx
@@ -10,34 +10,32 @@ const SpeechBubbleHeader = () => {
   const searchParams = useSearchParams();
 
   return (
-    <section>
-      <div className='relative'>
-        <div className='absolute z-10 w-full'>
-          <IsBackHeader href={`./?${searchParams.toString()}`} />
-          <Text sizes='40' weight='700' className='absolute right-[6rem] top-[7rem] text-gray-200'>
-            {'"'}
-          </Text>
-          <FlexBox justifyContent='center' className='text-center'>
-            <Text sizes='16' weight='500'>
-              연간{' '}
-              <Text sizes='16' weight='800' className='text-active'>
-                약 550,020원
-              </Text>
-              의 혜택을 <br /> 받을 수 있어요
+    <section className='relative'>
+      <div className='absolute z-10 w-full'>
+        <IsBackHeader href={`./?${searchParams.toString()}`} />
+        <Text sizes='40' weight='700' className='absolute right-[6rem] top-[5rem] text-gray-200'>
+          {'"'}
+        </Text>
+        <FlexBox justifyContent='center' className='text-center'>
+          <Text sizes='16' weight='500'>
+            연간{' '}
+            <Text sizes='16' weight='800' className='text-active'>
+              약 550,020원
             </Text>
-          </FlexBox>
-          <Text sizes='40' weight='700' className='absolute left-[6rem] top-[7rem] text-gray-200'>
-            {'"'}
+            의 혜택을 <br /> 받을 수 있어요
           </Text>
-        </div>
-        <Image
-          src='/images/speech-bubble.png'
-          alt='말풍선'
-          width={530}
-          height={290}
-          className='absolute top-0 w-full'
-        />
+        </FlexBox>
+        <Text sizes='40' weight='700' className='absolute left-[6rem] top-[5rem] text-gray-200'>
+          {'"'}
+        </Text>
       </div>
+      <Image
+        src='/images/speech-bubble.png'
+        alt='말풍선'
+        width={530}
+        height={290}
+        className='absolute top-[-3.6rem] w-full'
+      />
     </section>
   );
 };
