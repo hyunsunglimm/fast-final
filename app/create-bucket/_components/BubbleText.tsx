86b21097bb9c391b88caac10effea0066347c568
oyjbaam <oyjbaam@gmail.com>
2024-05-28 16:33:15 +0900

    feat: 상단 말풍선 영역

--- app/create-bucket/_components/BubbleText.tsx
+++ app/create-bucket/_components/BubbleText.tsx
@@ -2,47 +2,14 @@ import React from 'react';
 import Text from '@/components/ui/Text';
 import FlexBox from '@/components/ui/FlexBox';
 import { cn } from '@/utils/twMerge';
+import { getStepConfig } from '../createBucketConfig';
 
 type BubbleTextProps = {
   currentStep: string | undefined;
 };
-type CreateBucketStep = {
-  title: string;
-  stepText: string;
-  imgSrc: string;
-};
-type CreateBucketConfigType = { [key: number]: CreateBucketStep };
-
-export const createBucketConfig: CreateBucketConfigType = {
-  0: {
-    title: '당신의 버킷리스트는 무엇인가요?',
-    stepText: '목표설정',
-    imgSrc: '/images/create-bucket/flow-1.png'
-  },
-  1: {
-    title: '출금통장과 저축통장을 선택해주세요',
-    stepText: '통장설정',
-    imgSrc: '/images/create-bucket/flow-2.png'
-  },
-  2: {
-    title: '매주 저축할 금액과 날짜를 선택해주세요',
-    stepText: '규칙설정',
-    imgSrc: '/images/create-bucket/flow-3.png'
-  },
-  3: {
-    title: '단순 저축으로만 달성하기 힘들다면 <br/> 이용중인 상품도 포함시켜보세요',
-    stepText: '상품연결',
-    imgSrc: '/images/create-bucket/flow-4.png'
-  }
-};
-
-export const getStepConfig = (step: string | undefined): CreateBucketStep => {
-  const stepIndex = step ? Number(step) : 0;
-  return createBucketConfig[stepIndex];
-};
 
 const BubbleText = ({ currentStep }: BubbleTextProps) => {
-  const stepIndex = currentStep ? Number(currentStep) : 0;
+  const stepIndex = Number(currentStep);
   const { title, stepText } = getStepConfig(currentStep);
 
   return (
@@ -54,11 +21,11 @@ const BubbleText = ({ currentStep }: BubbleTextProps) => {
     >
       <FlexBox alignItems='center'>
         {[...Array(4)].map((_, idx) => {
-          const isActiveStep = stepIndex === idx;
+          const isActiveStep = stepIndex === idx + 1;
           const currentStepWidthClass = isActiveStep
             ? ' px-[0.8rem] py-[0.4rem]'
             : 'w-[1.6rem] h-[1.6rem] ';
-          const overStepBgClass = stepIndex >= idx ? 'bg-gray-700' : 'bg-gray-300';
+          const overStepBgClass = stepIndex >= idx + 1 ? 'bg-gray-700' : 'bg-gray-300';
 
           return (
             <React.Fragment key={idx}>
@@ -71,7 +38,7 @@ const BubbleText = ({ currentStep }: BubbleTextProps) => {
                   overStepBgClass
                 )}
               >
-                {stepIndex === idx && <Text sizes='12'>{stepText}</Text>}
+                {stepIndex === idx + 1 && <Text sizes='12'>{stepText}</Text>}
               </FlexBox>
             </React.Fragment>
           );
