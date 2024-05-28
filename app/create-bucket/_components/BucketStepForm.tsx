f586db14ed6221ef78845a9beffe9281950c6377
oyjbaam <oyjbaam@gmail.com>
2024-05-29 01:35:20 +0900

    feat: 버킷 폼 입력값에 따라 url 바뀌게 수정

--- app/create-bucket/_components/BucketStepForm.tsx
+++ app/create-bucket/_components/BucketStepForm.tsx
@@ -1,16 +1,36 @@
+'use client';
 import React from 'react';
 import { StepOne, StepTwo, StepFour } from './step';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import NextButton from './NextButton';
+import { debounce } from '@/utils/debounce';
+
 type BucketStepFormProps = {
   currentStep: string | undefined;
 };
 
 const BucketStepForm = ({ currentStep }: BucketStepFormProps) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleChangeQueryString = debounce((query: string, term: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (term) {
+      params.set(query, term);
+    } else {
+      params.delete(query);
+    }
+    replace(`${pathname}?${params.toString()}`, { scroll: false });
+  }, 300);
+
   return (
-    <form className='mb-24 mt-24 space-y-[0.8rem]'>
-      {currentStep === '1' && <StepOne />}
-      {currentStep === '2' && <StepTwo />}
-      {currentStep === '3' && <StepTwo />}
-      {currentStep === '4' && <StepFour />}
+    <form className='mt-24 flex flex-col gap-y-8'>
+      {currentStep === '1' && <StepOne handleChangeQueryString={handleChangeQueryString} />}
+      {currentStep === '2' && <StepTwo handleChangeQueryString={handleChangeQueryString} />}
+      {currentStep === '3' && <StepTwo handleChangeQueryString={handleChangeQueryString} />}
+      {currentStep === '4' && <StepFour handleChangeQueryString={handleChangeQueryString} />}
+      <NextButton currentStep={currentStep} />
     </form>
   );
 };
