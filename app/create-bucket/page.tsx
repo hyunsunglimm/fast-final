4713566c20b8e8048ec78a350902ca755e704fdb
oyjbaam <oyjbaam@gmail.com>
2024-05-28 16:32:47 +0900

    feat: 버킷 생성 페이지 레이아웃

---
+++ app/create-bucket/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import BucketFlowHeader from './_components/BucketFlowHeader';
+import BubbleText from './_components/BubbleText';
+import PorkoImage from './_components/PorkoImage';
+import NextButton from './_components/NextButton';
+import BucketStepForm from './_components/BucketStepForm';
+
+type CreateBucketPageProps = {
+  searchParams: Record<string, string | undefined>;
+};
+
+const CreateBucketPage = ({ searchParams }: CreateBucketPageProps) => {
+  const currentStep = searchParams.step;
+
+  return (
+    <>
+      <BucketFlowHeader currentStep={currentStep} />
+      <BubbleText currentStep={currentStep} />
+      <PorkoImage currentStep={currentStep} />
+      <BucketStepForm currentStep={currentStep} />
+      <NextButton currentStep={currentStep} />
+    </>
+  );
+};
+export default CreateBucketPage;
