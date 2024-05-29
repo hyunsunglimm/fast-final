7f32805a31b604b5954571ddfeeeb4748e947865
jiyoon29 <yoon__29@naver.com>
2024-05-29 14:04:53 +0900

    feat: Title 의 justifyContent type을 FlexBox type으로 가져오는걸로 변경

--- app/budget-calendar/_components/common/Title.tsx
+++ app/budget-calendar/_components/common/Title.tsx
@@ -1,11 +1,12 @@
 import React, { ReactNode } from 'react';
-import FlexBox from '@/components/ui/FlexBox';
+import FlexBox, { flexBoxVariants } from '@/components/ui/FlexBox';
+import { VariantProps } from 'class-variance-authority';
 
 type TitleProps = {
   title: string;
   children?: ReactNode;
   className?: string;
-  justifyContent?: 'start' | 'center' | 'end' | 'between' | 'around' | 'evenly';
+  justifyContent?: VariantProps<typeof flexBoxVariants>['justifyContent'];
 };
 
 const Title: React.FC<TitleProps> = ({ title, children, className, justifyContent }) => {
