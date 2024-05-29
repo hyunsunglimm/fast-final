3ea8fe19f7278cab40c18cfff6449252689eeb2f
oyjbaam <oyjbaam@gmail.com>
2024-05-30 03:59:17 +0900

    feat: result page 데이터 작성

--- app/create-bucket/result/page.tsx
+++ app/create-bucket/result/page.tsx
@@ -3,7 +3,13 @@ import Text from '@/components/ui/Text';
 import Image from 'next/image';
 import FlexBox from '@/components/ui/FlexBox';
 import Button from '@/components/ui/Button';
-const CreateBucketListResult = () => {
+import { QueryType } from '../_components/BucketStepForm';
+
+type CreateBucketListResultProps = {
+  searchParams: Record<Partial<QueryType>, string | undefined>;
+};
+
+const CreateBucketListResult = ({ searchParams }: CreateBucketListResultProps) => {
   return (
     <section className='mt-[5.2rem] flex w-full flex-col items-center justify-center px-20'>
       <Text variant='h1' sizes='24' weight='700' className='text-center'>
@@ -14,31 +20,49 @@ const CreateBucketListResult = () => {
         width={172}
         height={201}
         alt='버킷 완성 포코 이미지'
-        className='h-[20.1rem] w-[17.205rem]'
+        className='mt-32 h-[20.1rem] w-[17.205rem]'
       />
       <FlexBox flexDirection='col' className='mb-[7.5rem] mt-40 w-full px-16'>
         <FlexBox alignItems='center' justifyContent='between' className='w-full py-8'>
           <Text variant='h5'>버킷리스트</Text>
-          <Text variant='h6' className=' inline-block rounded-full bg-white px-16 py-8'>
-            친구랑 유럽 여행가기
+          <Text
+            variant='h6'
+            weight='500'
+            className=' inline-block rounded-full bg-white px-16 py-8'
+          >
+            {searchParams['bucket-name']}
           </Text>
         </FlexBox>
         <FlexBox alignItems='center' justifyContent='between' className='w-full py-8'>
           <Text variant='h5'>저축주기</Text>
-          <Text variant='h6' className=' inline-block rounded-full bg-white px-16 py-8'>
-            친구랑 유럽 여행가기
+          <Text
+            variant='h6'
+            weight='500'
+            className=' inline-block rounded-full bg-white px-16 py-8'
+          >
+            매주 {searchParams['day-of-week']} {searchParams['savings-amount']}원씩
           </Text>
         </FlexBox>
         <FlexBox alignItems='center' justifyContent='between' className='w-full py-8'>
           <Text variant='h5'>상품연결</Text>
-          <Text variant='h6' className=' inline-block rounded-full bg-white px-16 py-8'>
-            친구랑 유럽 여행가기
+          <Text
+            variant='h6'
+            weight='500'
+            className=' inline-block rounded-full bg-white px-16 py-8'
+          >
+            {decodeURIComponent(searchParams['my-saving-product'] || '')
+              .split('|')
+              .filter((item) => item.trim() !== '').length + '개' || '0개'}
           </Text>
         </FlexBox>
         <FlexBox alignItems='center' justifyContent='between' className='w-full py-8'>
           <Text variant='h5'>목표금액</Text>
-          <Text variant='h6' className=' inline-block rounded-full bg-white px-16 py-8'>
-            친구랑 유럽 여행가기
+          <Text
+            variant='h6'
+            weight='500'
+            className=' inline-block rounded-full bg-white px-16 py-8'
+          >
+            {searchParams['target-amount']} 원
           </Text>
         </FlexBox>
       </FlexBox>
