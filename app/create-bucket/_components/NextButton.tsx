58760b3d4ed9a5bcc753752ccf940cd14a454a5e
oyjbaam <oyjbaam@gmail.com>
2024-05-29 23:39:53 +0900

    refactor: 버킷 플로우 다음 버튼 수정

--- app/create-bucket/_components/NextButton.tsx
+++ app/create-bucket/_components/NextButton.tsx
@@ -1,26 +1,53 @@
 'use client';
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import Button from '@/components/ui/Button';
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import { getLinkHref } from '../util';
 
-const NextButton = ({ currentStep }: { currentStep: string | undefined }) => {
+type NextButtonProps = {
+  currentStep: string | undefined;
+  buttonLabel: string;
+  disabled?: boolean;
+  type: HTMLButtonElement['type'];
+  asChild?: boolean;
+};
+
+const NextButton = ({
+  currentStep,
+  buttonLabel,
+  type = 'button',
+  disabled = false,
+  asChild
+}: NextButtonProps) => {
   const searchParams = useSearchParams();
   const transNumberStep = Number(currentStep);
   const linkHref = getLinkHref(transNumberStep, 1, searchParams);
 
+  const handleDisabledAnchorClick = (e: MouseEvent<HTMLAnchorElement>) => {
+    if (disabled) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <>
-      {transNumberStep < 4 ? (
-        <Button asChild size='lg' className='mt-24 w-full'>
-          <Link href={linkHref}>다음</Link>
-        </Button>
-      ) : (
-        <Button type='submit' size='lg' className='mt-24 w-full text-16 font-400 '>
-          완료
-        </Button>
-      )}
+      <Button
+        asChild={asChild}
+        disabled={disabled}
+        type={type}
+        size='lg'
+        styled='fill_black'
+        className='mt-24 w-full text-16 font-400'
+      >
+        {transNumberStep < 4 ? (
+          <Link href={linkHref} onClick={handleDisabledAnchorClick}>
+            {buttonLabel}
+          </Link>
+        ) : (
+          buttonLabel
+        )}
+      </Button>
     </>
   );
 };
