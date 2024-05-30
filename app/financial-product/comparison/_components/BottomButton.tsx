f3353dcb4821a02aab7cfe49a558a24ff7dfe828
hyunsunglimm <gustjd3834@naver.com>
2024-05-30 20:52:11 +0900

    feat: BottomButton에 클릭 핸들러 추가

--- app/financial-product/comparison/_components/BottomButton.tsx
+++ app/financial-product/comparison/_components/BottomButton.tsx
@@ -1,19 +1,30 @@
 import Button from '@/components/ui/Button';
-import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 type BottomButtonProps = {
-  title: string;
   path: string;
+  onClick?: () => void;
+  children: React.ReactNode;
 };
 
-const BottomButton = ({ title, path }: BottomButtonProps) => {
+const BottomButton = ({ path, onClick, children }: BottomButtonProps) => {
   const searchParams = useSearchParams();
+  const router = useRouter();
 
   return (
     <div className='fixed bottom-0 left-0 right-0 mx-auto bg-gradient-to-t from-gray-200 to-white/20 px-20 py-32 xs:w-[520px]'>
-      <Button size='lg' styled='fill_black' asChild>
-        <Link href={`${path}?${searchParams.toString()}`}>{title}</Link>
+      <Button
+        size='lg'
+        styled='fill_black'
+        onClick={() => {
+          if (onClick) {
+            onClick();
+          } else {
+            router.push(`${path}?${searchParams.toString()}`);
+          }
+        }}
+      >
+        {children}
       </Button>
     </div>
   );
