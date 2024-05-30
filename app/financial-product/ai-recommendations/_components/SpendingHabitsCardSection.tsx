c001b577fa549bfbb0cedfba79cd148367063803
hyunsunglimm <gustjd3834@naver.com>
2024-05-30 17:27:22 +0900

    feat: 소비습관에 맞는 카드 mock server 적용

--- app/financial-product/ai-recommendations/_components/SpendingHabitsCardSection.tsx
+++ app/financial-product/ai-recommendations/_components/SpendingHabitsCardSection.tsx
@@ -1,65 +1,28 @@
+'use client';
+
 import Icon from '@/components/Icon';
+import Spinner from '@/components/Spinner';
 import SwiperWrapper from '@/components/SwiperWrapper';
 import FlexBox from '@/components/ui/FlexBox';
 import Text from '@/components/ui/Text';
 import { Card } from '@/components/ui/card';
-
-const benefits = [
-  {
-    title: '대중교통',
-    iconPath: '/icons/system-icon/system/card-bus.svg',
-    content: (
-      <Text>
-        대중교통{' '}
-        <Text weight='700' className='text-primary'>
-          5%{' '}
-        </Text>
-        캐시백
-      </Text>
-    )
-  },
-  {
-    title: '카카오페이',
-    iconPath: '/icons/system-icon/system/card-pay.svg',
-    content: (
-      <Text>
-        카카오페이{' '}
-        <Text weight='700' className='text-primary'>
-          7%{' '}
-        </Text>
-        캐시백
-      </Text>
-    )
-  },
-  {
-    title: '스타벅스',
-    iconPath: '/icons/system-icon/system/card-cafe.svg',
-    content: (
-      <Text>
-        스타벅스{' '}
-        <Text weight='700' className='text-primary'>
-          5%{' '}
-        </Text>
-        캐시백
-      </Text>
-    )
-  },
-  {
-    title: '전월실적',
-    iconPath: '/icons/system-icon/system/card-performance.svg',
-    content: (
-      <Text>
-        전월실적{' '}
-        <Text weight='700' className='text-primary'>
-          25만원{' '}
-        </Text>
-        이상
-      </Text>
-    )
-  }
-];
+import { getSpendingHabitsCards } from '@/service/api/financial-product/cards';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 
 const SpendingHabitsCardSection = () => {
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const {
+    data: spendingHabitsCards,
+    isPending,
+    isSuccess
+  } = useQuery({
+    queryKey: ['spendingHabitsCards'],
+    queryFn: getSpendingHabitsCards
+  });
+
+  const currentCard = isSuccess ? spendingHabitsCards[currentCardIndex] : null;
+
   return (
     <section className='bg-white pb-[6rem] pt-40'>
       <Text variant='h2' sizes='20' weight='600' className='mb-[2.6rem] text-center'>
@@ -79,32 +42,47 @@ const SpendingHabitsCardSection = () => {
           가 높았어요!
         </Text>
       </div>
-      <SwiperWrapper dots coverflow>
-        <div className='h-[16rem] rounded-xs bg-[#FF7674]' />
-        <div className='h-[16rem] rounded-xs bg-[#87CFDE]' />
-        <div className='h-[16rem] rounded-xs bg-[#959498]' />
-        <div className='h-[16rem] rounded-xs bg-[#959]' />
-      </SwiperWrapper>
-      <FlexBox flexDirection='col' alignItems='center' className='mt-32 px-20'>
-        <Text sizes='12' className='mb-[0.2rem]'>
-          하나은행
-        </Text>
-        <Text sizes='18' weight='600' className='mb-16'>
-          카카오페이 CHECK
-        </Text>
-        <ul className='flex w-full flex-col gap-[0.6rem]'>
-          {benefits.map(({ title, iconPath, content }) => {
-            return (
-              <li key={title}>
-                <Card className='flex w-full items-center gap-[1.2rem] border border-gray-100 px-24 py-16'>
-                  <Icon src={iconPath} alt={title} size='20' />
-                  {content}
-                </Card>
-              </li>
-            );
-          })}
-        </ul>
-      </FlexBox>
+      {isPending || !isSuccess ? (
+        <FlexBox justifyContent='center'>
+          <Spinner />
+        </FlexBox>
+      ) : (
+        <>
+          <SwiperWrapper dots coverflow setIndex={setCurrentCardIndex}>
+            {spendingHabitsCards.map(({ title, image }) => (
+              <div key={title} className={`h-[16rem] rounded-xs ${image}`} />
+            ))}
+          </SwiperWrapper>
+          <FlexBox flexDirection='col' alignItems='center' className='mt-32 px-20'>
+            <Text sizes='12' className='mb-[0.2rem]'>
+              {currentCard?.title}
+            </Text>
+            <Text sizes='18' weight='600' className='mb-16'>
+              {currentCard?.cardName}
+            </Text>
+            <ul className='flex w-full flex-col gap-[0.6rem]'>
+              {currentCard?.benefits.map(({ title, iconPath, detail }) => {
+                const content = detail.split(' ');
+
+                return (
+                  <li key={title}>
+                    <Card className='flex w-full items-center gap-[1.2rem] border border-gray-100 px-24 py-16'>
+                      <Icon src={iconPath} alt={title} size='20' className='rounded-none' />
+                      <Text>
+                        {content[0]}{' '}
+                        <Text weight='700' className='text-primary'>
+                          {content[1]}{' '}
+                        </Text>
+                        {content[2]}
+                      </Text>
+                    </Card>
+                  </li>
+                );
+              })}
+            </ul>
+          </FlexBox>
+        </>
+      )}
     </section>
   );
 };
--- service/api/financial-product/cards.ts
+++ service/api/financial-product/cards.ts
@@ -1,6 +1,10 @@
-import { SpotlightCard } from '@/types/card';
+import { SpendingHabitsCard, SpotlightCard } from '@/types/card';
 import { requestFetch } from '../fetchOptions';
 
 export const getSpotlightCards = (type: string | null): Promise<SpotlightCard[]> => {
   return requestFetch(`/api/cards/spotlight?type=${type}`);
+};
+
+export const getSpendingHabitsCards = (): Promise<SpendingHabitsCard[]> => {
+  return requestFetch('/api/cards/spending-habits');
 };
