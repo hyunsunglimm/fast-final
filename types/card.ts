043c6940bb37350dbe830f882b2a009e0a675963
hyunsunglimm <gustjd3834@naver.com>
2024-05-30 20:51:51 +0900

    add: 비교된 카드 타이핑

--- types/card.ts
+++ types/card.ts
@@ -17,10 +17,23 @@ export type SpendingHabitsCard = {
   }[];
 };
 
-export type ComparisonCard = {
+export type CardsToCompare = {
   id: string;
   title: string;
   description: string;
   fileName: string;
   benefits: string[];
+};
+
+export type ComparedCards = {
+  id: number;
+  annualBenefit: number;
+  name: string;
+  monthlyBenefit: number;
+  discountLimit: string;
+  annualFee: string;
+  prevMonthPerformance: string;
+  benefits: string[];
+  diffByStandardA: string;
+  diffByStandardB: string;
 };
