2f9c6c66ba76f2fd9a4c6e438fda6a614fd745b6
hyunsunglimm <gustjd3834@naver.com>
2024-05-30 18:16:43 +0900

    fix: 정적 데이터 별도 관리

--- app/financial-product/theme-recommendations/page.tsx
+++ app/financial-product/theme-recommendations/page.tsx
@@ -2,37 +2,7 @@ import Text from '@/components/ui/Text';
 import Link from 'next/link';
 import { IsBackHeader } from '@/components/header';
 import Icon from '@/components/Icon';
-
-const Themes = [
-  {
-    title: '쇼핑 카드 추천',
-    subTitle: '쇼핑 중독자에게 꼭 맞는',
-    iconPath: '/icons/financial-product/shopping.svg',
-    color: 'bg-[#FF7D51]',
-    href: 'shopping-card'
-  },
-  {
-    title: '편의점 카드 추천',
-    subTitle: '편의점 단골 손님을 위한',
-    iconPath: '/icons/financial-product/convenience-store-card-recommendation.svg',
-    color: 'bg-[#4CC590]',
-    href: 'convenience-card'
-  },
-  {
-    title: '6개월 적금 추천',
-    subTitle: '다가올 여름 여행 경비를 위한',
-    iconPath: '/icons/financial-product/six-month-savings-recommendation.svg',
-    color: 'bg-[#65A4DA]',
-    href: 'six-month-savings'
-  },
-  {
-    title: '연말 ・ 연초 적금 상품 추천',
-    subTitle: '4%이상 금리를 받고싶은 당신을 위한',
-    iconPath: '/icons/financial-product/savings-recommendation.svg',
-    color: 'bg-[#8F98FF]',
-    href: 'savings'
-  }
-];
+import { THEMES } from '@/utils/financial-product/staticData';
 
 const ThemeRecommendationsPage = () => {
   return (
@@ -43,7 +13,7 @@ const ThemeRecommendationsPage = () => {
           상황에 맞는 상품을 <br /> 선택해 보세요!
         </Text>
         <ul className='flex flex-col gap-[1.6rem]'>
-          {Themes.map((theme) => {
+          {THEMES.map((theme) => {
             return (
               <li key={theme.title}>
                 <Link
