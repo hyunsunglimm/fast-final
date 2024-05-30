d1ce35be16f02090a04a26223ee210090f63896d
hyunsunglimm <gustjd3834@naver.com>
2024-05-30 21:43:49 +0900

    fix: 테마별 추천 색상 미출력 이슈

--- utils/financial-product/staticData.ts
+++ utils/financial-product/staticData.ts
@@ -64,28 +64,28 @@ export const THEMES = [
     title: '쇼핑 카드 추천',
     subTitle: '쇼핑 중독자에게 꼭 맞는',
     iconPath: '/icons/financial-product/shopping.svg',
-    color: 'bg-[#FF7D51]',
+    color: '#FF7D51',
     href: 'shopping-card'
   },
   {
     title: '편의점 카드 추천',
     subTitle: '편의점 단골 손님을 위한',
     iconPath: '/icons/financial-product/convenience-store-card-recommendation.svg',
-    color: 'bg-[#4CC590]',
+    color: '#4CC590',
     href: 'convenience-card'
   },
   {
     title: '6개월 적금 추천',
     subTitle: '다가올 여름 여행 경비를 위한',
     iconPath: '/icons/financial-product/six-month-savings-recommendation.svg',
-    color: 'bg-[#65A4DA]',
+    color: '#65A4DA',
     href: 'six-month-savings'
   },
   {
     title: '연말 ・ 연초 적금 상품 추천',
     subTitle: '4%이상 금리를 받고싶은 당신을 위한',
     iconPath: '/icons/financial-product/savings-recommendation.svg',
-    color: 'bg-[#8F98FF]',
+    color: '#8F98FF',
     href: 'savings'
   }
 ];
