56031bdecef39b983ac39cfa6b3430ffa882b555
hyunsunglimm <gustjd3834@naver.com>
2024-05-30 15:33:16 +0900

    feat: mock server test 적용

--- app/financial-product/ai-recommendations/_components/PerfectFinancialProductSection.tsx
+++ app/financial-product/ai-recommendations/_components/PerfectFinancialProductSection.tsx
@@ -1,52 +1,15 @@
-'use client';
-
 import FlexBox from '@/components/ui/FlexBox';
 import Tab from '@/components/ui/Tab';
 import Text from '@/components/ui/Text';
 import SavingCard from '../../_components/SavingCard';
 import Button from '@/components/ui/Button';
 import Icon from '@/components/Icon';
+import { getSavings } from '@/service/api/financial-product/savings';
 
-const savings = [
-  {
-    bank: '카카오뱅크',
-    title: '26주 적금',
-    description: '캐릭터와 함께 즐거운 도전',
-    rate: '연 6%',
-    iconPath: '/icons/logos/bank/bank-kakao.svg',
-    startAmount: '5000원',
-    endAmount: 1772807
-  },
-  {
-    bank: '기업은행',
-    title: 'IBK D-day 적금',
-    description: '만기가 자유로운 적금',
-    rate: '연 5.15%',
-    iconPath: '/icons/logos/bank/bank-ibk.svg',
-    startAmount: '월 20만원',
-    endAmount: 1215249
-  },
-  {
-    bank: '카카오뱅크',
-    title: '굴비 적금',
-    description: '부담없이 이용하는 적금',
-    rate: '연 5%',
-    iconPath: '/icons/logos/bank/bank-kakao.svg',
-    startAmount: '월 30만원',
-    endAmount: 1822207
-  },
-  {
-    bank: '농협은행',
-    title: 'NH올원e미니적금',
-    description: '금액과 기간을 자유롭게',
-    rate: '연 4.7%',
-    iconPath: '/icons/logos/bank/bank-nh.svg',
-    startAmount: '월 50만원',
-    endAmount: 3034792
-  }
-];
+const PerfectFinancialProductSection = async () => {
+  await new Promise((res) => setTimeout(res, 2000));
+  const savings = await getSavings();
 
-const PerfectFinancialProductSection = () => {
   return (
     <section className='px-20 py-40'>
       <FlexBox justifyContent='between' alignItems='center'>
--- app/financial-product/ai-recommendations/page.tsx
+++ app/financial-product/ai-recommendations/page.tsx
@@ -2,6 +2,9 @@ import { IsBackHeader } from '@/components/header';
 import AiBanner from './_components/AiBanner';
 import PerfectFinancialProductSection from './_components/PerfectFinancialProductSection';
 import SpendingHabitsCardSection from './_components/SpendingHabitsCardSection';
+import { Suspense } from 'react';
+import FlexBox from '@/components/ui/FlexBox';
+import Spinner from '@/components/Spinner';
 
 const AiRecommendationsPage = () => {
   return (
@@ -9,7 +12,15 @@ const AiRecommendationsPage = () => {
       <IsBackHeader title='AI 맞춤' href='./?tab=신용카드' defaultColor='#f2f4f6' />
       <main className='bg-gray-50'>
         <AiBanner />
-        <PerfectFinancialProductSection />
+        <Suspense
+          fallback={
+            <FlexBox justifyContent='center'>
+              <Spinner />
+            </FlexBox>
+          }
+        >
+          <PerfectFinancialProductSection />
+        </Suspense>
         <SpendingHabitsCardSection />
       </main>
     </>
