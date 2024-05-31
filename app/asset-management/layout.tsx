0422e7b5d19d020730e95fbd36a81df9190a415f
oyjbaam <oyjbaam@gmail.com>
2024-06-01 02:29:27 +0900

    feat: 메타데이터

--- app/asset-management/layout.tsx
+++ app/asset-management/layout.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { DefaultHeader } from '@/components/header';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'PORKO 자산관리 페이지',
+  description: '자산관리 페이지'
+};
 
 const AssetLayout = ({ children }: { children: React.ReactNode }) => {
   return (
