317d5ee3d70dea8352adc9d9086b2b335b11c252
jiyoon29 <yoon__29@naver.com>
2024-06-03 03:10:13 +0900

    feat: 지출 데이터 배열을 매핑하여 코드를 재구성함
    - api 연결 시 데이터 구조의 변경을 고려하여 코드 재구성 필요

--- app/budget-calendar/consumption/_components/SpendingCategories.tsx
+++ app/budget-calendar/consumption/_components/SpendingCategories.tsx
@@ -2,106 +2,123 @@ import React from 'react';
 import Title from '../../_components/common/Title';
 import Tab from '@/components/ui/Tab';
 import Text from '@/components/ui/Text';
-import Icon from '@/components/Icon';
+import Icon, { IconProps } from '@/components/Icon';
 import FlexBox from '@/components/ui/FlexBox';
-import IconDot from '../../_components/common/IconDot';
+import IconDot, { dotVariants } from '../../_components/common/IconDot';
+import { VariantProps } from 'class-variance-authority';
+
+type SpendingData = {
+  name: string;
+  percentage: string;
+  amount: string;
+  dotColor: Exclude<VariantProps<typeof dotVariants>['color'], null>;
+  position: string;
+  size: string;
+  color: string;
+  icon: string;
+  iconSize?: IconProps['size'];
+};
+
+const spendingData: SpendingData[] = [
+  {
+    name: '교통',
+    percentage: '32%',
+    amount: '1,000,000원',
+    dotColor: 'blue',
+    position: 'left-[4.77rem]',
+    size: '12rem',
+    color: '#D8F8FF',
+    icon: '/icons/categories/background/categories-4.svg',
+    iconSize: '48'
+  },
+  {
+    name: '교육/학습',
+    percentage: '21%',
+    amount: '1,000,000원',
+    dotColor: 'green',
+    position: 'left-[13.77rem] bottom-0',
+    size: '9.6rem',
+    color: '#DDF7A6',
+    icon: '/icons/categories/background/categories-8.svg',
+    iconSize: '48'
+  },
+  {
+    name: '경조/선물',
+    percentage: '14%',
+    amount: '1,000,000원',
+    dotColor: 'red',
+    position: 'left-[17.97rem] top-[1.15rem]',
+    size: '7.2rem',
+    color: '#FFE5E0',
+    icon: '/icons/categories/background/categories-9.svg',
+    iconSize: '32'
+  },
+  {
+    name: '의료/건강',
+    percentage: '8%',
+    amount: '1,000,000원',
+    dotColor: 'red',
+    position: 'right-[4.77rem] top-[8.25rem]',
+    size: '5.76rem',
+    color: '#FFE5E0',
+    icon: '/icons/categories/background/categories-10.svg',
+    iconSize: '28.8'
+  },
+  {
+    name: '패션/뷰티/미용',
+    percentage: '7%',
+    amount: '1,000,000원',
+    dotColor: 'yellow',
+    position: 'left-[7.203rem] bottom-[2.28rem]',
+    size: '4.8rem',
+    color: '#FFF4CF',
+    icon: '/icons/categories/background/categories-3.svg',
+    iconSize: '28.8'
+  }
+];
 
 const SpendingCategories = () => {
   return (
     <div className='px-20 py-32'>
       <Title title='지출 비교' className='mb-16' />
       <Tab array={['금액', '건수']} type='box' tabKey='veiwType' />
-      <ul className='relative my-40 min-h-[20.2rem]'>
-        <li className='absolute left-[4.77rem] flex h-[12rem] w-[12rem] flex-col items-center justify-center rounded-full bg-[#D8F8FF]'>
-          <Icon src='/icons/categories/background/categories-4.svg' alt='교통' size='48' />
-          <Text className='text-gray-600'>32%</Text>
-        </li>
-        <li className='absolute bottom-0 left-[13.77rem] flex h-[9.6rem] w-[9.6rem] flex-col items-center justify-center rounded-full bg-[#DDF7A6]'>
-          <Icon src='/icons/categories/background/categories-8.svg' alt='책' size='48' />
-          <Text className='text-gray-600'>21%</Text>
-        </li>
-        <li className='absolute left-[17.97rem] top-[1.15rem] flex h-[7.2rem] w-[7.2rem] flex-col items-center justify-center rounded-full bg-[#FFE5E0]'>
-          <Icon src='/icons/categories/background/categories-9.svg' alt='교통' size='32' />
-          <Text className='text-gray-600'>14%</Text>
-        </li>
-        <li className='absolute right-[4.77rem] top-[8.25rem] flex h-[5.76rem] w-[5.76rem] flex-col items-center justify-center rounded-full bg-[#FFE5E0]'>
-          <Icon src='/icons/categories/background/categories-10.svg' alt='교통' size='28.8' />
-        </li>
-        <li className='absolute bottom-[2.28rem] left-[7.203rem] flex h-[4.8rem] w-[4.8rem] flex-col items-center justify-center rounded-full bg-[#FFF4CF]'>
-          <Icon src='/icons/categories/background/categories-3.svg' alt='교통' size='28.8' />
-        </li>
-      </ul>
+      <div className='relative my-40 min-h-[20.2rem]'>
+        {spendingData.map((item, index) => {
+          const sizeClass = `w-[${item.size}] h-[${item.size}]`;
+          const bgColor = `bg-[${item.color}]`;
+          return (
+            <FlexBox
+              key={index}
+              flexDirection='col'
+              alignItems='center'
+              justifyContent='center'
+              className={`absolute ${item.position} ${sizeClass} rounded-full ${bgColor}`}
+            >
+              <Icon src={item.icon} alt={item.name} size={item.iconSize} />
+              {index < 3 && <Text className='text-gray-600'>{item.percentage}</Text>}
+            </FlexBox>
+          );
+        })}
+      </div>
       <ul className='px-16'>
-        <li>
-          <FlexBox justifyContent='between' className='py-8'>
-            <FlexBox alignItems='center'>
-              <IconDot color='blue' />
-              <Text variant='h4' weight='700' className='mr-8'>
-                교통
-              </Text>
-              <Text className='text-gray-500' sizes='12'>
-                32%
-              </Text>
-            </FlexBox>
-            <Text weight='700'>1,000,000원</Text>
-          </FlexBox>
-        </li>
-        <li>
-          <FlexBox justifyContent='between' className='py-8'>
-            <FlexBox alignItems='center'>
-              <IconDot color='green' />
-              <Text variant='h4' weight='700' className='mr-8'>
-                교육/학습
-              </Text>
-              <Text className='text-gray-500' sizes='12'>
-                21%
-              </Text>
-            </FlexBox>
-            <Text weight='700'>1,000,000원</Text>
-          </FlexBox>
-        </li>
-        <li>
-          <FlexBox justifyContent='between' className='py-8'>
-            <FlexBox alignItems='center'>
-              <IconDot color='red' />
-              <Text variant='h4' weight='700' className='mr-8'>
-                경조/선물
-              </Text>
-              <Text className='text-gray-500' sizes='12'>
-                14%
-              </Text>
-            </FlexBox>
-            <Text weight='700'>1,000,000원</Text>
-          </FlexBox>
-        </li>
-        <li>
-          <FlexBox justifyContent='between' className='py-8'>
-            <FlexBox alignItems='center'>
-              <IconDot color='red' />
-              <Text variant='h4' weight='700' className='mr-8'>
-                의료/건강
-              </Text>
-              <Text className='text-gray-500' sizes='12'>
-                8%
-              </Text>
-            </FlexBox>
-            <Text weight='700'>1,000,000원</Text>
-          </FlexBox>
-        </li>
-        <li>
-          <FlexBox justifyContent='between' className='py-8'>
-            <FlexBox alignItems='center'>
-              <IconDot color='yellow' />
-              <Text variant='h4' weight='700' className='mr-8'>
-                패션/뷰티/미용
-              </Text>
-              <Text className='text-gray-500' sizes='12'>
-                7%
-              </Text>
-            </FlexBox>
-            <Text weight='700'>1,000,000원</Text>
-          </FlexBox>
-        </li>
+        {spendingData.map((item, index) => {
+          return (
+            <li key={index}>
+              <FlexBox justifyContent='between' className='py-8'>
+                <FlexBox alignItems='center'>
+                  <IconDot color={item.dotColor} />
+                  <Text variant='h4' weight='700' className='mr-8'>
+                    {item.name}
+                  </Text>
+                  <Text className='text-gray-500' sizes='12'>
+                    {item.percentage}
+                  </Text>
+                </FlexBox>
+                <Text weight='700'>{item.amount}</Text>
+              </FlexBox>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
