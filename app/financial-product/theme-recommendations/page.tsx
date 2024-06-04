396ec1b310b42232aca3209e6c05a6c088f0ebe2
hyunsunglimm <gustjd3834@naver.com>
2024-06-04 16:55:28 +0900

    fix: 테마별 추천페이지 padding top 추가

--- app/financial-product/theme-recommendations/page.tsx
+++ app/financial-product/theme-recommendations/page.tsx
@@ -8,7 +8,7 @@ const ThemeRecommendationsPage = () => {
   return (
     <>
       <IsBackHeader href='./?tab=신용카드' defaultColor='#fff' />
-      <main className='bg-white px-20 pb-[6rem]'>
+      <main className='bg-white px-20 pb-[6rem] pt-[1rem]'>
         <Text variant='h1' sizes='24' weight='600' className='mb-40'>
           상황에 맞는 상품을 <br /> 선택해 보세요!
         </Text>
