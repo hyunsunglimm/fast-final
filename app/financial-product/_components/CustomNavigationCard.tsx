fa252520f9f07a7d86622972652f423e94864921
hyunsunglimm <gustjd3834@naver.com>
2024-06-04 16:43:16 +0900

    fix: 맞춤탐색 UT반영 디자인 수정

--- app/financial-product/_components/CustomNavigationCard.tsx
+++ app/financial-product/_components/CustomNavigationCard.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import Button from '@/components/ui/Button';
+import FlexBox from '@/components/ui/FlexBox';
 import Text from '@/components/ui/Text';
-import { Card } from '@/components/ui/card/Card';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -10,29 +9,30 @@ type CustomNavigationCardProps = {
   recommendationInfo: {
     title: string;
     subTitle: string;
-    description: string;
-    linkTitle: string;
     href: string;
+    color: string;
   };
 };
 
 const CustomNavigationCard = ({
-  recommendationInfo: { title, subTitle, description, linkTitle, href }
+  recommendationInfo: { title, subTitle, href, color }
 }: CustomNavigationCardProps) => {
   const imageUrl = href.split('?')[0];
 
   return (
-    <Card className='w-full shrink-0 p-24'>
-      <Text variant='h2' sizes='12' weight='700' className='mb-[1rem] text-primary'>
-        {title}
-      </Text>
-      <Text variant='p' sizes='20' weight='600' className='mb-[0.4rem]'>
-        {subTitle}
-      </Text>
-      <Text variant='p' sizes='14' className='text-gray-700'>
-        {description}
-      </Text>
-      <div className='my-[3.6rem] flex justify-center'>
+    <Link
+      style={{ backgroundColor: color }}
+      href={`/financial-product/${href}`}
+      aria-label={`${title} 이동`}
+      className='h-[30.9rem] w-[25.1rem] shrink-0 rounded-[2rem] px-24 py-28 text-white'
+    >
+      <FlexBox flexDirection='col' className='mb-[5.3rem] gap-4'>
+        <Text sizes='18' weight='700'>
+          {title}
+        </Text>
+        <Text>{subTitle}</Text>
+      </FlexBox>
+      <FlexBox justifyContent='center'>
         <Image
           src={`/images/financial-product/${imageUrl}.webp`}
           alt='search products'
@@ -41,13 +41,34 @@ const CustomNavigationCard = ({
           priority
           className='pointer-events-none h-[14.2rem] w-auto'
         />
-      </div>
-      <Button asChild size='md'>
-        <Link href={`/financial-product/${href}`} aria-label={`${title} 이동`} scroll={false}>
-          {linkTitle}
-        </Link>
-      </Button>
-    </Card>
+      </FlexBox>
+    </Link>
+    // <Card className='w-full shrink-0 p-24'>
+    //   <Text variant='h2' sizes='12' weight='700' className='mb-[1rem] text-primary'>
+    //     {title}
+    //   </Text>
+    //   <Text variant='p' sizes='20' weight='600' className='mb-[0.4rem]'>
+    //     {subTitle}
+    //   </Text>
+    //   <Text variant='p' sizes='14' className='text-gray-700'>
+    //     {description}
+    //   </Text>
+    //   <div className='my-[3.6rem] flex justify-center'>
+    //     <Image
+    //       src={`/images/financial-product/${imageUrl}.webp`}
+    //       alt='search products'
+    //       width={300}
+    //       height={142}
+    //       priority
+    //       className='pointer-events-none h-[14.2rem] w-auto'
+    //     />
+    //   </div>
+    //   <Button asChild size='md'>
+    //     <Link href={`/financial-product/${href}`} aria-label={`${title} 이동`}>
+    //       {linkTitle}
+    //     </Link>
+    //   </Button>
+    // </Card>
   );
 };
 
--- app/financial-product/_components/CustomNavigationSection.tsx
+++ app/financial-product/_components/CustomNavigationSection.tsx
@@ -1,6 +1,6 @@
 import Text from '@/components/ui/Text';
 import CustomNavigationCard from './CustomNavigationCard';
-import SwiperWrapper from '@/components/SwiperWrapper';
+import FlexBox from '@/components/ui/FlexBox';
 import { FINANCIAL_PRODUCT_NAVIGATION } from '@/shared/utils/financial-product/staticData';
 
 const CustomNavigationSection = () => {
@@ -9,11 +9,11 @@ const CustomNavigationSection = () => {
       <Text sizes='20' variant='h1' weight='700' className='mb-20'>
         맞춤탐색
       </Text>
-      <SwiperWrapper dots>
+      <FlexBox className='hide-scrollbar gap-20 overflow-x-scroll'>
         {FINANCIAL_PRODUCT_NAVIGATION.map((info) => (
           <CustomNavigationCard key={info.title} recommendationInfo={info} />
         ))}
-      </SwiperWrapper>
+      </FlexBox>
     </section>
   );
 };
