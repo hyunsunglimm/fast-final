dc977445d853aa48784c506a00bd4201d3443eff
hyunsunglimm <gustjd3834@naver.com>
2024-06-04 18:17:17 +0900

    fix: isBackHeader에 X아이콘 사용할 수 있게 isClose프롭 추가했습니다.

--- components/header/IsBackHeader.tsx
+++ components/header/IsBackHeader.tsx
@@ -5,15 +5,23 @@ import Text from '../ui/Text';
 import Icon from '../Icon';
 import Link from 'next/link';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import IconButton from '../ui/IconButton';
 
 type HeaderProps = {
   title?: string;
   href: string;
   defaultColor?: string;
   isFixed?: boolean;
+  isClose?: boolean;
 };
 
-export const IsBackHeader = ({ title, href, defaultColor, isFixed = true }: HeaderProps) => {
+export const IsBackHeader = ({
+  title,
+  href,
+  defaultColor,
+  isFixed = true,
+  isClose = false
+}: HeaderProps) => {
   const { scrollY } = useScroll();
 
   const headerColor = useTransform(
@@ -31,15 +39,32 @@ export const IsBackHeader = ({ title, href, defaultColor, isFixed = true }: Head
         className: `${isFixed && 'sticky top-0 z-20'} h-[5.6rem] border-b border-b-gray-100 px-20 backdrop-blur-lg`
       })}
     >
-      <Link href={href} aria-label='뒤로 가기 링크' scroll={false}>
-        <Icon aria-hidden size='24' alt='뒤로가기' src='/icons/system-icon/arrow/arrow-prev.svg' />
-      </Link>
+      {!isClose ? (
+        <Link href={href} aria-label='뒤로 가기 링크' scroll={false}>
+          <Icon
+            aria-hidden
+            size='24'
+            alt='뒤로가기'
+            src='/icons/system-icon/arrow/arrow-prev.svg'
+          />
+        </Link>
+      ) : (
+        <div className='w-[2.4rem]' aria-hidden></div>
+      )}
       <FlexBox justifyContent='center'>
         <Text variant='h6' sizes='16' weight='700'>
           {title}
         </Text>
       </FlexBox>
-      <div className='w-[2.4rem]' aria-hidden></div>
+      {isClose ? (
+        <IconButton asChild>
+          <Link href={href} aria-label={`${href}로 이동`}>
+            <Icon src='/icons/system-icon/x.svg' alt='취소 아이콘' size='20' aria-hidden />
+          </Link>
+        </IconButton>
+      ) : (
+        <div className='w-[2.4rem]' aria-hidden></div>
+      )}
     </motion.header>
   );
 };
