030846e63492a5eba76636b6b5fabe33f1ea3604
hyunsunglimm <gustjd3834@naver.com>
2024-06-04 18:58:14 +0900

    add: 버킷 랜딩페이지 추가

---
+++ app/bucket-landing/page.tsx
@@ -0,0 +1,16 @@
+import { IsBackHeader } from '@/components/header';
+
+const BucketLandingPage = () => {
+  return (
+    <>
+      <IsBackHeader href='/create-bucket' title='이용안내' defaultColor='#fff' isClose />
+      <main>
+        <section>
+          <p>asd</p>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default BucketLandingPage;
