bef7dec9563981e4f279bb90edef1d1bc2facd12
jiyoon29 <yoon__29@naver.com>
2024-06-05 12:09:32 +0900

    feat: 날짜 dropdown component

--- app/budget-calendar/_components/look-alone/ExpensCalendarBox.tsx
+++ app/budget-calendar/_components/look-alone/ExpensCalendarBox.tsx
@@ -1,63 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import Title from '../common/Title';
 import Icon from '@/components/Icon';
 import FlexBox from '@/components/ui/FlexBox';
 import Link from 'next/link';
 import Button from '@/components/ui/Button';
 import Calendar from '../shared/Calendar';
+import YearMonthDropdown from '../shared/YearMonthDropdown';
 
 const ExpensCalendarBox = () => {
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
   const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1);
-  const [isOpen, setIsOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement | null>(null);
-
-  const handleToggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
 
   const handleYearMonthSelect = (year: number, month: number) => {
     setSelectedYear(year);
     setSelectedMonth(month);
-    setIsOpen(false);
-  };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-      setIsOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
-  const renderOptions = () => {
-    const options = [];
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth() + 1;
-    for (let i = 0; i < 12; i++) {
-      const year = currentYear - Math.floor((i + currentMonth - 1) / 12);
-      let month = (currentMonth + 12 - i) % 12;
-      if (month === 0) {
-        month = 12;
-      }
-      options.push({ year, month });
-    }
-    return options.map(({ year, month }) => {
-      return (
-        <li
-          key={`${year}-${month}`}
-          onClick={() => handleYearMonthSelect(year, month)}
-          className='py-4'
-        >
-          {String(year).slice(2)}년 {month}월
-        </li>
-      );
-    });
   };
 
   return (
@@ -66,24 +22,11 @@ const ExpensCalendarBox = () => {
         <Icon src='/icons/system-icon/info.svg' alt='정보' size='16' />
       </Title>
       <FlexBox justifyContent='between' alignItems='center' className='relative mb-24'>
-        <div className='relative' ref={dropdownRef}>
-          <FlexBox alignItems='center' onClick={handleToggleDropdown} className='text-16 font-700'>
-            {selectedYear !== new Date().getFullYear()
-              ? `${String(selectedYear).slice(2)}년 `
-              : null}
-            {selectedMonth}월
-            <Icon
-              src='/icons/system-icon/arrow/bxs-down-arrow.svg'
-              alt='화살표'
-              className='ml-2 h-[0.6rem] w-[0.8rem] rounded-none'
-            />
-          </FlexBox>
-          {isOpen && (
-            <ul className='absolute z-[2] w-[10rem] rounded-sm border bg-white px-4 py-8 text-center text-14'>
-              {renderOptions()}
-            </ul>
-          )}
-        </div>
+        <YearMonthDropdown
+          selectedYear={selectedYear}
+          selectedMonth={selectedMonth}
+          onSelect={handleYearMonthSelect}
+        />
         <Button asChild size='xs' styled='outline'>
           <Link
             href='/budget-calendar/consumption?daily=월별&viewType=금액'
