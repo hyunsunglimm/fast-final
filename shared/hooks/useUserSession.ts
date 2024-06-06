87b87d1b4649ce8f4a583643fc6b65a4fb501834
oyjbaam <oyjbaam@gmail.com>
2024-06-06 23:29:30 +0900

    refactor: 유저 데이터 수정

--- shared/hooks/useUserSession.ts
+++ shared/hooks/useUserSession.ts
@@ -1,8 +1,8 @@
 import { useSession } from 'next-auth/react';
 
 // 클라이언트 컴포넌트에서 token 호출 Hook
-export const useUserToken = () => {
+export const useUserSession = () => {
   const session = useSession();
 
-  return session.data?.user?.accessToken;
+  return session.data?.user;
 };
