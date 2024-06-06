35700cab84a2e6346577b1abb9bfebf8b44be37e
oyjbaam <oyjbaam@gmail.com>
2024-06-06 23:12:56 +0900

    feat: 미들웨어 경로 추가

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,58 @@
 import { NextResponse, NextRequest } from 'next/server';
+import { protectedRoutes, authRedirectRoutes, apiAuthPrefix } from './routes';
+import { currentUserSession } from './shared/actions/auth';
+import { match } from 'path-to-regexp';
 
 /**
  * 인증 및 권한 부여 ,서버 측 리디렉션, 경로 재작성
  */
-const middleware = (request: NextRequest) => {
-  /** /create-bucket으로 접속할 경우 step=1로 주소 바꿈 */
-  if (request.nextUrl.pathname === '/create-bucket' && !request.nextUrl.searchParams.get('step')) {
-    return NextResponse.redirect(new URL('/create-bucket?step=1', request.url));
+const middleware = async (request: NextRequest) => {
+  const { nextUrl } = request;
+  const session = await currentUserSession();
+  const isLoggedIn = !!session;
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+  if (isApiAuthRoute) {
+    return NextResponse.next();
   }
-  /** /bucket-landing으로 접속할 경우 tab=저축생활+1편으로 주소 바꿈 */
-  if (request.nextUrl.pathname === '/bucket-landing' && !request.nextUrl.searchParams.get('tab')) {
-    return NextResponse.redirect(new URL('/bucket-landing?tab=저축생활+1편', request.url));
+
+  // 로그인이 필요한 경로에 대한 처리
+  if (isMatch(nextUrl.pathname, protectedRoutes)) {
+    if (!isLoggedIn) {
+      return NextResponse.redirect(new URL('/auth/login', nextUrl));
+    }
+    return NextResponse.next();
+  }
+
+  // 로그인 한 사용자가 접근하면 홈으로 리디렉션
+  if (isMatch(nextUrl.pathname, authRedirectRoutes)) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL('/', nextUrl));
+    }
+    return NextResponse.next();
+  }
+
+  // /create-bucket으로 접속할 경우 step=1로 주소 바꿈
+  if (nextUrl.pathname === '/create-bucket') {
+    if (!nextUrl.searchParams.get('step')) {
+      return NextResponse.redirect(new URL('/create-bucket?step=1', nextUrl));
+    }
+    return NextResponse.next();
+  }
+
+  // /bucket-landing으로 접속할 경우 tab=저축생활+1편으로 주소 바꿈
+  if (nextUrl.pathname === '/bucket-landing') {
+    if (!nextUrl.searchParams.get('tab')) {
+      return NextResponse.redirect(new URL('/bucket-landing?tab=저축생활+1편', nextUrl));
+    }
+    return NextResponse.next();
   }
 };
 export default middleware;
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
 };
+
+const isMatch = (pathname: string, urls: string[]) =>
+  urls.some((url) => Boolean(match(url)(pathname)));
