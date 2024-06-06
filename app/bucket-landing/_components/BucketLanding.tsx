0eff26b62e4f14430cc8ea8bcb3d5f7e077e5140
oyjbaam <oyjbaam@gmail.com>
2024-06-06 19:25:54 +0900

    refactor: 버킷 랜딩 페이지 헤더 위치 수정, 말풍선 이미지 교체

--- app/bucket-landing/_components/BucketLanding.tsx
+++ app/bucket-landing/_components/BucketLanding.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { IsBackHeader } from '@/components/header';
 import BucketLandingPartOne from './BucketLandingPartOne';
 import { useQueryString } from '@/shared/hooks/useQueryString';
 import BucketLandingPartTwo from './BucketLandingPartTwo';
@@ -9,16 +8,13 @@ import Tab from '@/components/ui/Tab';
 const BucketLanding = () => {
   const { queryValue } = useQueryString();
   return (
-    <>
-      <IsBackHeader href='/create-bucket' title='이용안내' defaultColor='#fff' isClose />
-      <main>
-        <div className='bg-white'>
-          <Tab array={['저축생활 1편', '저축생활 2편']} type='underline' tabKey='tab' />
-        </div>
-        {queryValue('tab') === '저축생활 1편' && <BucketLandingPartOne />}
-        {queryValue('tab') === '저축생활 2편' && <BucketLandingPartTwo />}
-      </main>
-    </>
+    <main>
+      <div className='bg-white'>
+        <Tab array={['저축생활 1편', '저축생활 2편']} type='underline' tabKey='tab' />
+      </div>
+      {queryValue('tab') === '저축생활 1편' && <BucketLandingPartOne />}
+      {queryValue('tab') === '저축생활 2편' && <BucketLandingPartTwo />}
+    </main>
   );
 };
 
