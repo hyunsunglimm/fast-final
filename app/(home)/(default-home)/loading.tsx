cb8faeba546ecd8bbe695fde4be46b08439c2016
oyjbaam <oyjbaam@gmail.com>
2024-06-06 20:14:07 +0900

    refactor: home loading 수정

--- app/(home)/(default-home)/loading.tsx
+++ app/(home)/(default-home)/loading.tsx
@@ -3,12 +3,14 @@ import { SqureSkeleton, CardSkeleton } from '@/components/ui/skeleton';
 import LoadingBackdrop from '@/components/ui/LoadingBackdrop';
 const loading = () => {
   return (
-    <div className='relative flex h-dvh flex-col gap-20 px-20'>
+    <>
       <LoadingBackdrop />
-      <CardSkeleton />
-      <SqureSkeleton />
-      <CardSkeleton />
-    </div>
+      <div className='relative space-y-8 px-20'>
+        <CardSkeleton />
+        <SqureSkeleton />
+        <CardSkeleton />
+      </div>
+    </>
   );
 };
 export default loading;
