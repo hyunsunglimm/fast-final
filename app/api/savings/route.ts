65227bfb9f9e4af0d3a0c17e72beae09c0fcb5f7
hyunsunglimm <gustjd3834@naver.com>
2024-06-10 20:31:45 +0900

    fix: 적금상품 정적데이터 변경

--- app/api/savings/route.ts
+++ app/api/savings/route.ts
@@ -12,21 +12,11 @@ const savings = [
     period: '6.5개월'
   },
   {
-    bank: '기업은행',
-    title: 'IBK D-day 적금',
-    description: '만기가 자유로운 적금',
-    rate: '연 5.15%',
-    iconPath: '/icons/logos/bank/bank-ibk.svg',
-    startAmount: '월 20만원',
-    endAmount: 1215249,
-    period: '6개월'
-  },
-  {
-    bank: '카카오뱅크',
+    bank: '토스',
     title: '굴비 적금',
     description: '부담없이 이용하는 적금',
     rate: '연 5%',
-    iconPath: '/icons/logos/bank/bank-kakao.svg',
+    iconPath: '/icons/logos/bank/bank-toss.svg',
     startAmount: '월 30만원',
     endAmount: 1822207,
     period: '6개월'
@@ -37,8 +27,18 @@ const savings = [
     description: '금액과 기간을 자유롭게',
     rate: '연 4.7%',
     iconPath: '/icons/logos/bank/bank-nh.svg',
-    startAmount: '월 50만원',
-    endAmount: 3034792,
+    startAmount: '월 30만원',
+    endAmount: 1828870,
+    period: '6개월'
+  },
+  {
+    bank: '기업은행',
+    title: 'IBK D-day 적금',
+    description: '만기가 자유로운 적금',
+    rate: '연 5.15%',
+    iconPath: '/icons/logos/bank/bank-ibk.svg',
+    startAmount: '월 20만원',
+    endAmount: 1215249,
     period: '6개월'
   }
 ];
