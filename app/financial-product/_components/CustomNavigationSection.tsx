5a1690cdbe510648fea455d394de557d052827df
hyunsunglimm <gustjd3834@naver.com>
2024-06-11 00:11:07 +0900

    feat: 금융상품의 맞춤탐색 드래깅 수정중

--- app/financial-product/_components/CustomNavigationSection.tsx
+++ app/financial-product/_components/CustomNavigationSection.tsx
@@ -1,19 +1,41 @@
+'use client';
+
 import Text from '@/components/ui/Text';
 import CustomNavigationCard from './CustomNavigationCard';
-import FlexBox from '@/components/ui/FlexBox';
 import { FINANCIAL_PRODUCT_NAVIGATION } from '@/shared/utils/financial-product/staticData';
+import { motion } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
+import { useWindowResize } from '@/shared/hooks/useWindowResize';
 
 const CustomNavigationSection = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [contentWidth, setContentWidth] = useState(0);
+  const { windowWidth } = useWindowResize();
+
+  useEffect(() => {
+    if (containerRef.current) {
+      setContainerWidth(containerRef.current.offsetWidth);
+      setContentWidth(containerRef.current.scrollWidth);
+    }
+  }, [windowWidth]);
+
   return (
     <section className='mb-40'>
       <Text sizes='20' variant='h1' weight='700' className='mb-20'>
         맞춤탐색
       </Text>
-      <FlexBox className='hide-scrollbar gap-20 overflow-x-scroll'>
-        {FINANCIAL_PRODUCT_NAVIGATION.map((info) => (
-          <CustomNavigationCard key={info.title} recommendationInfo={info} />
-        ))}
-      </FlexBox>
+      <div className='w-full scroll-px-20 overflow-hidden'>
+        <motion.div
+          drag='x'
+          dragConstraints={{ left: -(contentWidth - containerWidth), right: 0 }}
+          className='hide-scrollbar flex gap-20 overflow-x-scroll'
+        >
+          {FINANCIAL_PRODUCT_NAVIGATION.map((info) => (
+            <CustomNavigationCard key={info.title} recommendationInfo={info} />
+          ))}
+        </motion.div>
+      </div>
     </section>
   );
 };
