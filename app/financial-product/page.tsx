5ce9ed7a0580ee6279d470ab814fa2e2c43675bb
hyunsunglimm <gustjd3834@naver.com>
2024-06-11 16:55:32 +0900

    feat: 금융상품 홈 쿼리스트링 보호

--- app/financial-product/page.tsx
+++ app/financial-product/page.tsx
@@ -4,13 +4,22 @@ import FinancialProductBanner from './_components/FinancialProductBanner';
 import SpotlightCardSection from './_components/SpotlightCardSection';
 import FinancialProductSection from './_components/FinancialProductSection';
 import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'PORKO 금융상품 탐색',
   description: 'PORKO 금융상품 탐색 페이지'
 };
 
-const FinancialProductPage = () => {
+type FinancialProductPageProps = {
+  searchParams: Record<string, string | undefined>;
+};
+
+const FinancialProductPage = ({ searchParams }: FinancialProductPageProps) => {
+  if (!searchParams.tab) {
+    return redirect('/financial-product?tab=신용카드');
+  }
+
   return (
     <>
       <DefaultHeader title='상품탐색' defaultColor='#f2f4f6' />
