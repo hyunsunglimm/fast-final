df9e5d05ecf5948f238fd993716564a75e7b67ce
jiyoon29 <yoon__29@naver.com>
2024-06-12 02:51:55 +0900

    fix: 함께봐요 계속 로딩... 되는 부분 해결

--- app/budget-calendar/(defaultHeaderLayout)/page.tsx
+++ app/budget-calendar/(defaultHeaderLayout)/page.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Tab from '@/components/ui/Tab';
 import LookAloneContainer from '../_components/look-alone/LookAloneContainer';
 import LookTogetherContainer from '../_components/look-together/LookTogetherContainer';
+import { currentUserSession } from '@/shared/actions/auth';
 
 type BudgetCalendarPageProps = {
   searchParams: Record<string, string | undefined>;
 };
 
-const BudgetCalendarPage = ({ searchParams }: BudgetCalendarPageProps) => {
+const BudgetCalendarPage = async ({ searchParams }: BudgetCalendarPageProps) => {
   const viewMode = searchParams.viewMode || '혼자봐요';
+  const session = await currentUserSession();
 
   return (
     <main className='min-h-full bg-white pb-[13.2rem]'>
@@ -21,7 +23,9 @@ const BudgetCalendarPage = ({ searchParams }: BudgetCalendarPageProps) => {
         />
       </div>
       {viewMode === '혼자봐요' && <LookAloneContainer />}
-      {viewMode === '함께봐요' && <LookTogetherContainer viewMode={viewMode} />}
+      {viewMode === '함께봐요' && (
+        <LookTogetherContainer userData={session?.user} viewMode={viewMode} />
+      )}
     </main>
   );
 };
