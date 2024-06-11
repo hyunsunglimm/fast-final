af4f835e1ba2332ce2a3666e5c68e944adf38daf
oyjbaam <oyjbaam@gmail.com>
2024-06-11 23:07:14 +0900

    refactor: 홈 위젯 server tanstackquery로 변경

--- service/api/home/action.ts
+++ service/api/home/action.ts
@@ -1,10 +1,7 @@
-'use server';
 import { EidtWidgetBodyType } from '@/shared/types/response/widgetResponse';
 import { requestFetch } from '../fetchOptions';
-import { revalidatePath } from 'next/cache';
 const BASE_URL = process.env.NEXT_PUBLIC_DEV_URL || '';
 
 export const putEidtWidgetItem = async (body: EidtWidgetBodyType) => {
-  revalidatePath('/');
   return requestFetch('/member/widget', { method: 'put', body: JSON.stringify(body) }, BASE_URL);
 };
