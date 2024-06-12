5d99f15f3be639efc1395a69fb3735b6a174a2bc
hyunsunglimm <gustjd3834@naver.com>
2024-06-12 10:12:13 +0900

    feat: 버킷랜딩페이지 헤더에 탭 포함

--- app/bucket-landing/layout.tsx
+++ app/bucket-landing/layout.tsx
@@ -1,4 +1,9 @@
-import { IsBackHeader } from '@/components/header';
+import Icon from '@/components/Icon';
+import FlexBox from '@/components/ui/FlexBox';
+import IconButton from '@/components/ui/IconButton';
+import Tab from '@/components/ui/Tab';
+import Text from '@/components/ui/Text';
+import Link from 'next/link';
 
 type BucketLadingLayoutProps = {
   children: React.ReactNode;
@@ -7,7 +12,23 @@ type BucketLadingLayoutProps = {
 const BucketLadingLayout = ({ children }: BucketLadingLayoutProps) => {
   return (
     <>
-      <IsBackHeader href='/' title='이용안내' isClose defaultColor='#fff' />
+      <header className='sticky top-0 z-20 bg-white'>
+        <FlexBox justifyContent='between' alignItems='center' className='h-[5.6rem] bg-white px-20'>
+          <div className='w-[2.4rem]' aria-hidden />
+          <Text variant='h6' sizes='16' weight='700'>
+            이용안내
+          </Text>
+          <IconButton
+            asChild
+            className='cursor-pointer active:scale-95 active:ring-1 active:ring-active active:ring-offset-1'
+          >
+            <Link href='/' aria-label='홈으로 이동'>
+              <Icon src='/icons/system-icon/x.svg' alt='취소 아이콘' size='20' aria-hidden />
+            </Link>
+          </IconButton>
+        </FlexBox>
+        <Tab srollTop array={['저축생활 1편', '저축생활 2편']} type='underline' tabKey='tab' />
+      </header>
       {children}
     </>
   );
