f1275688d3b0eceb113f5306250231b7d11d16c2
hyunsunglimm <gustjd3834@naver.com>
2024-06-12 10:12:49 +0900

    feat: 버킷 랜딩페이지 QA 수정사항 적용

--- app/bucket-landing/_components/BucketLanding.tsx
+++ app/bucket-landing/_components/BucketLanding.tsx
@@ -3,15 +3,11 @@
 import BucketLandingPartOne from './BucketLandingPartOne';
 import { useQueryString } from '@/shared/hooks/useQueryString';
 import BucketLandingPartTwo from './BucketLandingPartTwo';
-import Tab from '@/components/ui/Tab';
 
 const BucketLanding = () => {
   const { queryValue } = useQueryString();
   return (
     <main>
-      <div className='bg-white'>
-        <Tab array={['저축생활 1편', '저축생활 2편']} type='underline' tabKey='tab' />
-      </div>
       {queryValue('tab') === '저축생활 1편' && <BucketLandingPartOne />}
       {queryValue('tab') === '저축생활 2편' && <BucketLandingPartTwo />}
     </main>
