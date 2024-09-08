61787dd15874509b42ed74ba9ba9f25be50e8544
hyunsunglimm <gustjd3834@naver.com>
2024-09-08 11:50:51 +0900

    add: 검색된 결과 없음 이미지 추가

--- app/api/cards/comparison/result/route.ts
+++ app/api/cards/comparison/result/route.ts
@@ -6,7 +6,7 @@ export const GET = async (request: Request) => {
   const cardIds = searchParams.get('card')?.split(',');
 
   if (typeof cardIds !== 'object') {
-    return new Response('bad request1', { status: 400 });
+    return new Response('bad request', { status: 400 });
   }
 
   const comparisonCards = await getSelectedCardsById(cardIds);
